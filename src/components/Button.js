4b7340d58193a7ab84cc3b9188137387071afcc2
Sanoj Shrestha <sanoj.shrestha.13@gmail.com>
2022-07-05 21:43:48 +0545

    onClick

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,18 @@
 import PropTypes from 'prop-types'
 const Button = ({ text, color }) => {
+    const onClick = () => {
+        console.log('MIMOOOOOOOO');
+    }
     return (
-        <button className="btn" style={{ backgroundColor: color }}>{text}</button>
+        <button onClick={onClick} className="btn" style={{ backgroundColor: color }}>{text}</button>
     )
 }
 Button.defaultProps = {
     color: 'steelblue',
 }
 Button.propTypes = {
-    text: PropTypes.string
+    text: PropTypes.string,
+    color: PropTypes.string,
+    onClick: PropTypes.func, 
 }
 export default Button
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import Button from "./Button"
 const Header = ({ title }) => {
+    const onClick = () => {
+        console.log('click');
+    }
     return (
         <header className="header">
             <h1>{title}</h1>
-            <Button color='green' text='YO' />
+            <Button color='green' text='YO' onClick={onClick} />
         </header >
     )
 }
