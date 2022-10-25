48b03eb39edc55cd247bc14bd53ce4207be4b4e7
Sanoj Shrestha <sanoj.shrestha.13@gmail.com>
2022-10-25 17:00:26 +0545

    added delete

--- src/App.js
+++ src/App.js
@@ -23,10 +23,13 @@ const App = () => {
       reminder: false,
     },
   ])
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      { tasks.length > 0 ?  <Tasks tasks={tasks} onDelete={deleteTask} /> : 'There are  no tasks.'}
     </div>
   );
 }
